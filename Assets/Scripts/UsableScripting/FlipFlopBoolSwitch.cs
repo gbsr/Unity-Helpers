756c5b76f0b8616b72b6a83a86093751ae93dbc5
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2018-06-11 16:37:04 +0200

    added script which describes how to do a flipFlop-switch (like the one in UE4)

---
+++ Assets/Scripts/UsableScripting/FlipFlopBoolSwitch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// How to write a FlipFlop-switch which works like the FlipFlop-node in Unreal Engine 4.
+/// </summary>
+public class FlipFlopBoolSwitch : MonoBehaviour
+{
+	private bool test;
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			print("LAST STATE: " + test);
+
+			//Here's where the magic actually happens.
+			test = !test;
+
+			print("NEW STATE: " + test);
+
+			if (test)
+				print("Do TRUE stuff!");
+			else
+				print("Do FALSE stuff!");
+		}
+	}
+}
