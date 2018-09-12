698d9dbf9807322452ff815d47d720997186f730
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2018-09-12 23:42:09 +0200

    better screenshotmaker

--- Assets/Scripts/ScreenshotMaker.cs
+++ Assets/Scripts/ScreenshotMaker.cs
@@ -10,6 +10,9 @@ public class ScreenshotMaker : MonoBehaviour
 	//Consider setting this to the property "Application.productName" in Awake().
 	public string yourGameName = string.Empty;
 
+	//The path where your Screenshots will be saved.
+	public static string DataPath { get { return Application.persistentDataPath + "/Screenshots/"; } }
+
 	[Tooltip("Choose a key to your liking here.")]
 	public KeyCode keyToDoScreenshotWith = KeyCode.F12;
 
@@ -19,10 +22,14 @@ public class ScreenshotMaker : MonoBehaviour
 	[Tooltip("Flip how the files should be named: FALSE = ScreenRes_Date, TRUE = Date_ScreenRes")]
 	public bool dateBeforeScreenRes = false;
 
-	/// <summary>
-	/// For doing higher resolution screenshots. Begins on "1" to not cause trouble when used in multiplication.
-	/// </summary>
+	/// <summary> For doing higher resolution screenshots. Begins on "1" to not cause trouble when used in multiplication. </summary>
 	public enum ResolutionOverloads { normalResolution = 1, doubleResolution, tripleResolution, quadrupleResolution }
+
+	private void Awake()
+	{
+		if (!System.IO.Directory.Exists(DataPath))
+			System.IO.Directory.CreateDirectory(DataPath);
+	}
 
 	private void Update()
 	{
@@ -31,28 +38,25 @@ public class ScreenshotMaker : MonoBehaviour
 	}
 
 	/// <summary>
-	/// Saves a screenshot into the application's Persistent Data-path.
+	/// Saves a Screenshot into the application's Persistent Data-path, in a "Screenshots"-folder.
 	/// Size will be the current screen resolution, and fileName will be marked by the current machine's date and time.
 	/// </summary>
 	private void CaptureScreen()
 	{
-		//Used to make a higher res version of the picture or not.
-		int resolutionMultiplier = (int)resolutionToUse;
-
-		//These variables do not affect the actual screenshot-resolution. They are used for setting the filename.
+		//These variables do not affect the actual Screenshot-resolution. They are used for setting the filename.
 		int widthString = Screen.width;
 		int heightString = Screen.height;
 
-		//Change depending on the value of the resolutionMultiplier, to give correct fileName.
-		widthString *= resolutionMultiplier;
-		heightString *= resolutionMultiplier;
+		//Change depending on the value of the resolutionToUse, to give correct fileName.
+		widthString *= (int)resolutionToUse;
+		heightString *= (int)resolutionToUse;
 
 		string fileName;
 		if (dateBeforeScreenRes)
 		{
 			//String concatenation courtesy of: https://answers.unity.com/questions/22954/how-to-save-a-picture-take-screenshot-from-a-camer.html
-			fileName = string.Format("{0}/{1}_Screenshot_{4}_{2}x{3}.png",
-								  Application.persistentDataPath,
+			fileName = string.Format("{0}{1}_Screenshot_{4}_{2}x{3}.png",
+								  DataPath,
 								  yourGameName,
 								  widthString,
 								  heightString,
@@ -60,8 +64,8 @@ public class ScreenshotMaker : MonoBehaviour
 		}
 		else
 		{
-			fileName = string.Format("{0}/{1}_Screenshot_{2}x{3}_{4}.png",
-								  Application.persistentDataPath,
+			fileName = string.Format("{0}{1}_Screenshot_{2}x{3}_{4}.png",
+								  DataPath,
 								  yourGameName,
 								  widthString,
 								  heightString,
@@ -69,7 +73,7 @@ public class ScreenshotMaker : MonoBehaviour
 		}
 
 		//Capture the screen and log it.
-		ScreenCapture.CaptureScreenshot(fileName, resolutionMultiplier);
+		ScreenCapture.CaptureScreenshot(fileName, (int)resolutionToUse);
 		Debug.Log("Saved screeshot at: " + fileName);
 	}
 }
