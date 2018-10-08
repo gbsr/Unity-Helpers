df0cf313dcc7de2a9f98aea89b7ad9b6be012023
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2018-10-08 19:20:48 +0200

    Added generic function for loading & spawning objects (in ResourcesLoader.cs)

--- Assets/Scripts/UsableScripting/ResourcesLoader.cs
+++ Assets/Scripts/UsableScripting/ResourcesLoader.cs
@@ -3,8 +3,43 @@
 /// <summary>
 /// Contains functions for loading an Object and get a sensible error if it failed.
 /// </summary>
-public class ResourcesLoader
+public class ResourcesLoader : MonoBehaviour
 {
+	public static void SpawnObject<T>(string resourcesPath, Vector3 spawnLocation, Quaternion spawnRotation) where T : Object
+	{
+		var objectToSpawnPrefab = LoadObject<T>(resourcesPath);
+
+		//Avoid errors of not correctly loaded Assets.
+		if (!objectToSpawnPrefab)
+			return;
+
+		//Spawn your object
+		var objectToSpawn = Instantiate(objectToSpawnPrefab, spawnLocation, spawnRotation);
+		Debug.Log("Object spawned: " + objectToSpawn.ToString());
+	}
+
+	/// <summary>
+	/// Generic function for loading any Type (eg. GameObjects, Sprites, Scripts) from the Resources-folder.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="resourcesPath"></param>
+	/// <returns></returns>
+	public static T LoadObject<T>(string resourcesPath) where T : UnityEngine.Object
+	{
+		var obj = (T)Resources.Load(resourcesPath);
+		if (obj)
+		{
+			return obj;
+		}
+		else
+		{
+			Debug.LogError("There exists no Asset with that path! \n" +
+				"Check if the Asset has been moved out of the Resources-folder. \n" +
+				"Desired Asset: " + resourcesPath);
+			return null;
+		}
+	}
+
 	/// <summary>
 	/// Returns a GameObject from the Resources-folder.
 	/// </summary>
@@ -17,8 +52,8 @@ public class ResourcesLoader
 		}
 		else
 		{
-			Debug.LogError("There exists no Asset with that path! " +
-				"Check if the Asset has been moved out of the Resources-folder. " +
+			Debug.LogError("There exists no Asset with that path! \n" +
+				"Check if the Asset has been moved out of the Resources-folder. \n" +
 				"Desired Asset: " + resourcesPath);
 			return null;
 		}
