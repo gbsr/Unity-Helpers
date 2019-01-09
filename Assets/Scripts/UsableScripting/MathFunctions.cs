ba5d7e2948e187f097fd39235d5ff3abc369f77f
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2019-01-10 00:58:41 +0100

    Added MathFunctions-script

---
+++ Assets/Scripts/UsableScripting/MathFunctions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// A collection of handy math functions, in addition to the ones present in "Mathf"
+/// </summary>
+public static class MathFunctions
+{
+	/// <summary>
+	/// Returns the nearest integer to the snapSteps-interval.
+	/// Original: https://www.reddit.com/r/Unity3D/comments/38bvns/round_to_even_number_or_nearest_5_in_c/
+	/// </summary>
+	public static float SnappedNumber(float input, float snapSteps = 10f)
+	{
+		if (snapSteps <= 0f)
+			throw new UnityException("factor argument must be above 0");
+
+		float snappedNumber = Mathf.Round(input / snapSteps) * snapSteps;
+
+		return snappedNumber;
+	}
+}
