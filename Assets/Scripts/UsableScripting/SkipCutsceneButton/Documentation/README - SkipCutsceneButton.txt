73eab08ccf14ce762dc3767903db1a00edb626e1
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2019-03-08 00:10:32 +0100

    Added documentation for the SkipCutsceneButton

---
+++ Assets/Scripts/UsableScripting/SkipCutsceneButton/Documentation/README - SkipCutsceneButton.txt
@@ -0,0 +1,20 @@
+This script can be used to create a "Skip Cutscene"-button, 
+ie. an UI-element with an icon for a button and a round progress bar which 
+increases while the player presses said button, and decreases when the button is released. 
+
+This script can also affect the scale of the button icon, making it becomen bigger or smaller
+when the player presses the button, and return to its initial size when the button is released.
+
+Keep in mind that you will *not* need Unity's UI Button-component for this to work.
+This script just affects a circle-shaped UI Image to simulate the progress bar-effect.
+
+***Hierarchy setup:***
+- SkipCutSceneButton [RectTransform] - 	[OPTIONAL] An empty parent with the script attached. 
+										Recommended to have, since it allows for resizing the entire button without 
+										messing with the direction of the size-adjustment performed on the HolderRect.
+	- HolderRect [RectTransform/Image] - Either an empty RectTransform or an Image acting as a background circle.
+		- FillImage [Image] - The progress bar. An Image-component (a circle sprite), set to ImageType = Filled.
+		- ButtonIconImage [Image] - [OPTIONAL] An icon of the button that this script is going to react to (could be anything really, this is just graphics).
+	
+***Source:***
+https://www.febucci.com/2019/02/skip-cutscenes-button/
--- Assets/Scripts/UsableScripting/SkipCutsceneButton/SkipCutsceneButton.cs
+++ Assets/Scripts/UsableScripting/SkipCutsceneButton/SkipCutsceneButton.cs
@@ -3,33 +3,38 @@ using UnityEngine.Assertions;
 using UnityEngine.UI;
 
 /// <summary>
-/// Image with fill up and size increase
-/// Source: https://www.febucci.com/2019/02/skip-cutscenes-button/
+/// Create a button icon which shows a progress bar and resizes when the player inputs the desired button/key (<see cref="activationKey"/>).
 /// <para></para>
-/// Hierarchy setup:
-/// - HolderRect - Either an empty RectTransform or an image acting as a background circle.
-///		- FillImage - The progress bar. An Image-component (a circle sprite), set to ImageType = Filled.
-///		- ButtonIconImage - [OPTIONAL] An icon of the button that this script is going to react to.
+/// Source: https://www.febucci.com/2019/02/skip-cutscenes-button/
 /// </summary>
+/// <remarks>
+/// Hierarchy setup:
+/// - SkipCutSceneButton[RectTransform] - [OPTIONAL] An empty parent with the script attached.
+/// 									  Recommended to have, since it allows for resizing the entire button without
+/// 									  messing with the direction of the size-adjustment performed on the HolderRect.
+/// 	- HolderRect[RectTransform / Image] - Either an empty RectTransform or an Image acting as a background circle.
+/// 		- FillImage[Image] - The progress bar.An Image-component(a circle sprite), set to ImageType = Filled.
+/// 		- ButtonIconImage[Image] - [OPTIONAL] An icon of the button that this script is going to react to (could be anything really, this is just graphics).
+///</remarks>
 public class SkipCutsceneButton : MonoBehaviour
 {
-	[Tooltip("Holder of the entire button, used to change its scale")]
-	public RectTransform holderRect;
-	[Tooltip("The circle that shows us the overall progress")]
+	[Tooltip("The circle that shows us the overall progress.")]
 	public Image fillImage;
+	[Tooltip("Holder of the button-graphics, used to change its scale when the 'activationKey' is pressed.")]
+	public RectTransform holderRect;
 
 	//Start scale of the holder
-	private Vector2 startHolderScale;
-	[Tooltip("The desired scale we want when we complete the input, i.e. the input percentage is 1")]
-	public Vector2 targetHoldercale = new Vector2(1.2f, 1.2f);
+	private Vector2 holderRectStartScale;
+	[Tooltip("The desired scale we want when we complete the input, i.e. the input percentage is 1.")]
+	public Vector2 holderRectTargetScale = new Vector2(1.2f, 1.2f);
 
-	[Tooltip("The key to start filling up the button.")]
+	[Tooltip("The key to press to start filling up the button.")]
 	public KeyCode activationKey = KeyCode.X;
 
-	[Tooltip("Speed that increases the progress if we press the desired key")]
+	[Range(0.01f, 5f)] [Tooltip("Speed that increases the progress if we press the desired 'activationKey'.")]
 	public float activeSpeed = 0.5f;
 
-	[Tooltip("How fast the progress decreases if we don't press the desired key")]
+	[Range(0.01f, 5f)] [Tooltip("How fast the progress decreases if we don't press the desired 'activationKey'.")]
 	public float cooldownSpeed = 0.6f;
 
 	//Prevents the function to be executed more than once
@@ -41,10 +46,9 @@ public class SkipCutsceneButton : MonoBehaviour
 		Assert.IsNotNull(fillImage);
 		Assert.IsNotNull(holderRect);
 
-		startHolderScale = holderRect.localScale;
+		//Cache 
+		holderRectStartScale = holderRect.localScale;
 		fillImage.fillAmount = 0;
-		fillImage.fillMethod = Image.FillMethod.Radial360;
-		fillImage.fillOrigin = 2;
 	}
 
 	private void Update()
@@ -58,8 +62,8 @@ public class SkipCutsceneButton : MonoBehaviour
 
 		//Changes the size of the button based on our progress
 		holderRect.localScale = Vector2.Lerp(
-			startHolderScale,
-			targetHoldercale,
+			holderRectStartScale,
+			holderRectTargetScale,
 			fillImage.fillAmount * fillImage.fillAmount //you can apply an interpolation there, I applied EaseIn
 			);
 
